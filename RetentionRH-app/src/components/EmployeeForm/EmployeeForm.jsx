6266fbea67c429b00955e34b57cc8b043e1c4c4d
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-12-31 03:31:37 +0100

    modifier la structure de page generate

--- RetentionRH-app/src/components/EmployeeForm/EmployeeForm.jsx
+++ RetentionRH-app/src/components/EmployeeForm/EmployeeForm.jsx
@@ -1,155 +1,312 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import './EmployeeForm.css';
 
+
+const sections = [
+  "Informations Personnelles",
+  "Parcours Professionnel & Éducation",
+  "Rôle & Satisfaction",
+  "Rémunération & Temps de Travail",
+  "Historique & Performance"
+];
+
 const EmployeeForm = ({ formData, onChange, onSubmit, loading }) => {
+  const [activeSection, setActiveSection] = useState(sections[0]);
+  const renderSection = () => {
+    switch (activeSection) {
+      case "Informations Personnelles":
+        return (
+          <div className="form-section">
+            <div className="form-row triple">
+              
+              <div className="form-field">
+                <label>Âge</label>
+                <input
+                  type="number"
+                  name="Age"
+                  min="18"
+                  max="70"
+                  value={formData.Age}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+              <div className="form-field">
+                <label>État Civil</label>
+                <select
+                  name="MaritalStatus"
+                  value={formData.MaritalStatus}
+                  onChange={onChange}
+                >
+                  <option value="Single">Célibataire</option>
+                  <option value="Married">Marié</option>
+                  <option value="Divorced">Divorcé</option>
+                </select>
+              </div>
+              <div className="form-field">
+                <label>Distance Maison-Travail (km)</label>
+                <input
+                  type="number"
+                  name="DistanceFromHome"
+                  min="1"
+                  value={formData.DistanceFromHome}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        );
+
+      case "Parcours Professionnel & Éducation":
+        return (
+          <div className="form-section">
+            <div className="form-row">
+              <div className="form-field">
+                <label>Département</label>
+                <input
+                  type="text"
+                  name="Department"
+                  value={formData.Department}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+              <div className="form-field">
+                <label>Domaine Éducation</label>
+                <input
+                  type="text"
+                  name="EducationField"
+                  value={formData.EducationField}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+              <div className="form-field">
+                <label>Business Travel</label>
+                <select
+                  name="BusinessTravel"
+                  value={formData.BusinessTravel}
+                  onChange={onChange}
+                >
+                  <option value="Non-Travel">Non-Travel</option>
+                  <option value="Travel_Rarely">Travel Rarely</option>
+                  <option value="Travel_Frequently">Travel Frequently</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        );
+
+      case "Rôle & Satisfaction":
+        return (
+          <div className="form-section">
+            <div className="form-row">
+              <div className="form-field">
+                <label>Rôle</label>
+                <input
+                  type="text"
+                  name="JobRole"
+                  value={formData.JobRole}
+                  onChange={onChange}
+                  required
+                />
+              </div>
+              <div className="form-field">
+                <label>Niveau de poste (1-5)</label>
+                <input
+                  type="number"
+                  name="JobLevel"
+                  min="1"
+                  max="5"
+                  value={formData.JobLevel}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-field">
+                <label>Satisfaction Job (1-4)</label>
+                <input
+                  type="number"
+                  name="JobSatisfaction"
+                  min="1"
+                  max="4"
+                  value={formData.JobSatisfaction}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Satisfaction Environnement (1-4)</label>
+                <input
+                  type="number"
+                  name="EnvironmentSatisfaction"
+                  min="1"
+                  max="4"
+                  value={formData.EnvironmentSatisfaction}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Implication (1-4)</label>
+                <input
+                  type="number"
+                  name="JobInvolvement"
+                  min="1"
+                  max="4"
+                  value={formData.JobInvolvement}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Work-Life Balance (1-4)</label>
+                <input
+                  type="number"
+                  name="WorkLifeBalance"
+                  min="1"
+                  max="4"
+                  value={formData.WorkLifeBalance}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </div>
+        );
+
+      case "Rémunération & Temps de Travail":
+        return (
+          <div className="form-section">
+            <div className="form-row">
+              <div className="form-field">
+                <label>Revenu Mensuel</label>
+                <input
+                  type="number"
+                  name="MonthlyIncome"
+                  value={formData.MonthlyIncome}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Taux Journalier</label>
+                <input
+                  type="number"
+                  name="DailyRate"
+                  value={formData.DailyRate}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Temps Supplémentaire</label>
+                <select
+                  name="OverTime"
+                  value={formData.OverTime}
+                  onChange={onChange}
+                >
+                  <option value="Yes">Oui</option>
+                  <option value="No">Non</option>
+                </select>
+              </div>
+              <div className="form-field">
+                <label>Stock Option Level (0-3)</label>
+                <input
+                  type="number"
+                  name="StockOptionLevel"
+                  min="0"
+                  max="3"
+                  value={formData.StockOptionLevel}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </div>
+        );
+
+      case "Historique & Performance":
+        return (
+          <div className="form-section">
+            <div className="form-row">
+              <div className="form-field">
+                <label>Total Working Years</label>
+                <input
+                  type="number"
+                  name="TotalWorkingYears"
+                  value={formData.TotalWorkingYears}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Training Times Last Year</label>
+                <input
+                  type="number"
+                  name="TrainingTimesLastYear"
+                  value={formData.TrainingTimesLastYear}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Years at Company</label>
+                <input
+                  type="number"
+                  name="YearsAtCompany"
+                  value={formData.YearsAtCompany}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Years in Current Role</label>
+                <input
+                  type="number"
+                  name="YearsInCurrentRole"
+                  value={formData.YearsInCurrentRole}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-field">
+                <label>Years with Current Manager</label>
+                <input
+                  type="number"
+                  name="YearsWithCurrManager"
+                  value={formData.YearsWithCurrManager}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
   return (
     <section className="form-card">
-      
-      <form onSubmit={onSubmit} className="professional-form">
-  
-  {/* SECTION 1 : IDENTITÉ & PERSONNEL */}
-  <div className="form-section">
-    <div className="form-section-header">
-      <h4>Identité & Personnel</h4>
-    </div>
-    <div className="form-row triple">
-      <div className="form-field">
-        <label>ID Employé</label>
-        <input type="number" name="employeeid" value={formData.employeeid} onChange={onChange} required />
+      {/* Barre verticale des onglets */}
+      <div className="form-tabs-vertical">
+        {sections.map(section => (
+          <button
+            type="button"
+            key={section}
+            className={activeSection === section ? "active-tab" : ""}
+            onClick={() => setActiveSection(section)}
+          >
+            {section}
+          </button>
+        ))}
       </div>
-      <div className="form-field">
-        <label>Âge</label>
-        <input type="number" name="Age" value={formData.Age} onChange={onChange} required />
-      </div>
-      <div className="form-field">
-        <label>État Civil</label>
-        <select name="MaritalStatus" value={formData.MaritalStatus} onChange={onChange}>
-          <option value="Single">Célibataire</option>
-          <option value="Married">Marié</option>
-          <option value="Divorced">Divorcé</option>
-        </select>
-      </div>
-    </div>
-  </div>
 
-  {/* SECTION 2 : POSTE & RÔLE */}
-  <div className="form-section">
-    <div className="form-section-header">
-      <h4>Poste & Rôle</h4>
-    </div>
-    <div className="form-row">
-      <div className="form-field">
-        <label>Rôle</label>
-        <select name="JobRole" value={formData.JobRole} onChange={onChange}>
-          <option value="Sales Executive">Sales Executive</option>
-          <option value="Research Scientist">Research Scientist</option>
-          <option value="Laboratory Technician">Laboratory Technician</option>
-          <option value="Manufacturing Director">Manufacturing Director</option>
-          <option value="Manager">Manager</option>
-          <option value="Sales Representative">Sales Representative</option>
-          <option value="Healthcare Representative">Healthcare Representative</option>
-          <option value="Healthcare Representative">Research Director</option>
-          <option value="Healthcare Representative">Human Resources</option>
-        </select>
-      </div>
-      <div className="form-field">
-        <label>Niveau de poste (1-5)</label>
-        <input type="number" name="JobLevel" min="1" max="5" value={formData.JobLevel} onChange={onChange} />
-      </div>
-    </div>
-    <div className="form-row">
-       <div className="form-field">
-        <label>Niveau d’implication dans le poste (1-4)</label>
-        <input type="number" name="JobInvolvement" min="1" max="4" value={formData.JobInvolvement} onChange={onChange} />
-      </div>
-      <div className="form-field">
-        <label>Niveau Stock Options (0-3)</label>
-        <input type="number" name="StockOptionLevel" min="0" max="3" value={formData.StockOptionLevel} onChange={onChange} />
-      </div>
-    </div>
-  </div>
+      {/* Contenu du formulaire */}
+      <div className="form-content">
+        <form onSubmit={onSubmit} className="professional-form">
+          {renderSection()}
 
-  {/* SECTION 3 : FINANCIER */}
-  <div className="form-section">
-    <div className="form-section-header">
-      <h4>Financier</h4>
-    </div>
-    <div className="form-row">
-      <div className="form-field">
-        <label>Revenu Mensuel ($)</label>
-        <input type="number" name="MonthlyIncome" value={formData.MonthlyIncome} onChange={onChange} />
+          <button type="submit" className="analyze-btn" disabled={loading}>
+            {loading ? "Analyse en cours..." : "Générer Plan de Rétention"}
+          </button>
+        </form>
       </div>
-      <div className="form-field">
-        <label>Taux Journalier (Daily Rate)</label>
-        <input type="number" name="DailyRate" value={formData.DailyRate} onChange={onChange} />
-      </div>
-    </div>
-  </div>
-
-  {/* SECTION 4 : ENVIRONNEMENT & DISTANCE */}
-  <div className="form-section">
-    <div className="form-section-header">
-      <h4>Environnement & Distance</h4>
-    </div>
-    <div className="form-row ">
-      <div className="form-field">
-        <label>Distance Maison-Travail (km)</label>
-        <input type="number" name="DistanceFromHome" value={formData.DistanceFromHome} onChange={onChange} />
-      </div>
-      <div className="form-field">
-        <label>Temps supplémentaire</label>
-        <select name="OverTime" value={formData.OverTime} onChange={onChange}>
-          <option value="No">Non</option>
-          <option value="Yes">Oui</option>
-        </select>
-      </div>
-      
-    </div>
-    <div className="form-row">
-      <div className="form-field">
-        <label>Satisfaction Job (1-4)</label>
-        <input type="number" name="JobSatisfaction" min="1" max="4" value={formData.JobSatisfaction} onChange={onChange} />
-      </div>
-      <div className="form-field">
-        <label>Satisfaction Environnement (1-4)</label>
-        <input type="number" name="EnvironmentSatisfaction" min="1" max="4" value={formData.EnvironmentSatisfaction} onChange={onChange} />
-      </div>
-    </div>
-  </div>
-
-  {/* SECTION 5 : EXPÉRIENCE & ANCIENNETÉ */}
-  <div className="form-section">
-    <div className="form-section-header">
-      <h4>Expérience & Ancienneté</h4>
-    </div>
-    <div className="form-row">
-      <div className="form-field">
-        <label>Total Années Expérience</label>
-        <input type="number" name="TotalWorkingYears" value={formData.TotalWorkingYears} onChange={onChange} />
-      </div>
-      <div className="form-field">
-        <label>Années dans l'entreprise</label>
-        <input type="number" name="YearsAtCompany" value={formData.YearsAtCompany} onChange={onChange} />
-      </div>
-    </div>
-    <div className="form-row">
-      <div className="form-field">
-        <label>Années poste actuel</label>
-        <input type="number" name="YearsInCurrentRole" value={formData.YearsInCurrentRole} onChange={onChange} />
-      </div>
-      <div className="form-field">
-        <label>Années avec Manager actuel</label>
-        <input type="number" name="YearsWithCurrManager" value={formData.YearsWithCurrManager} onChange={onChange} />
-      </div>
-    </div>
-  </div>
-
-  <button type="submit" className="analyze-btn" disabled={loading}>
-    {loading ? "Analyse en cours..." : "Générer Plan de Rétention"}
-  </button>
-</form>
     </section>
   );
 };
+
 
 export default EmployeeForm;
--- RetentionRH-app/src/pages/generate/generate.jsx
+++ RetentionRH-app/src/pages/generate/generate.jsx
@@ -4,174 +4,175 @@ import './generate.css';
 import LogoutButton from '../../components/LogoutButton/Logout';
 
 const Generate = () => {
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
-  };
+  const [activeSection, setActiveSection] = useState("Profil");
   const [formData, setFormData] = useState({
-    employeeid: 0,
-    Age: 30,
-    DailyRate: 800,
-    DistanceFromHome: 5,
-    EnvironmentSatisfaction: 3,
-    JobInvolvement: 3,
-    JobLevel: 1,
-    JobRole: "Sales Executive",
-    JobSatisfaction: 3,
+    Age: 41,
     MaritalStatus: "Single",
-    MonthlyIncome: 5000,
-    OverTime: "No",
+    DistanceFromHome: 1,
+    Department: "Sales",
+    EducationField: "Life Sciences",
+    BusinessTravel: "Travel_Rarely",
+    JobRole: "Sales Executive",
+    JobLevel: 2,
+    EnvironmentSatisfaction: 2,
+    JobInvolvement: 3,
+    JobSatisfaction: 4,
+    WorkLifeBalance: 1,
+    DailyRate: 1102,
+    MonthlyIncome: 5993,
+    OverTime: "Yes",
     StockOptionLevel: 0,
-    TotalWorkingYears: 5,
-    YearsAtCompany: 3,
-    YearsInCurrentRole: 2,
-    YearsWithCurrManager: 2,
+    TotalWorkingYears: 8,
+    TrainingTimesLastYear: 0,
+    YearsAtCompany: 6,
+    YearsInCurrentRole: 4,
+    YearsWithCurrManager: 5,
   });
 
   const [result, setResult] = useState(null);
-  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const handleGeneratePlan = async () => {
+  try {
+    setIsLoading(true);
+
+    await generatePlan();   
+
+    setActiveSection("Résultats");   
+  } finally {
+    setIsLoading(false);
+  }
+};
 
   const handleChange = (e) => {
-  setFormData({ ...formData, [e.target.name]: e.target.value });
-};
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleAnalysis = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError(null);
-   
-    
-
     const token = localStorage.getItem("token");
-    console.log("Valeur du token  :", token);
 
-    
     const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'token': token },
-    body: JSON.stringify(formData)
-         };
+      method: "POST",
+      headers: { "Content-Type": "application/json", token },
+      body: JSON.stringify(formData),
+    };
 
     try {
+      const [resPredict, resPlan] = await Promise.all([
+        fetch("http://127.0.0.1:8000/predict", options),
+        fetch("http://127.0.0.1:8000/generate-retention-plan", options),
+      ]);
 
-      const [resPredict ,resPlan ] = await Promise.all (
-        [fetch('http://127.0.0.1:8000/predict', options),
-        fetch(`http://127.0.0.1:8000/generate-retention-plan`,options)]
-      )
+      const resultPredict = await resPredict.json();
+      const resultPlan = await resPlan.json();
 
-
-     const resultPredict = await resPredict.json();
-     const resultPlan = await resPlan.json();
-    
-   
-      
-      if (resPredict.ok && resPlan.ok ) {
+      if (resPredict.ok && resPlan.ok) {
         setResult({
-         score: resultPredict.churn_probability * 100,
-         plan: resultPlan.retention_plan
-
-       });
-       console.log("Success")
-      } 
-
-      else {
-        
-       console.log("Détails de l'erreur (predict) :", resultPredict.detail);
-       console.log("Détails de l'erreur (predict) :", resultPlan.detail);
-       
-      setResult(null); 
-      alert("Erreur du serveur lors de l'analyse.")}
-
+          score: resultPredict.churn_probability * 100,
+          plan: resultPlan.retention_plan,
+        });
+      } else {
+        setResult(null);
+        alert("Erreur du serveur lors de l'analyse.");
+      }
     } catch (error) {
-    console.error("Erreur lors des appels simultanés", error);
-  
-     
+      console.error("Erreur lors des appels simultanés", error);
+      setResult(null);
     } finally {
       setLoading(false);
     }
   };
-  
+
   return (
-    <div className="predict-container">
+    <div className="generate-page">
       <header className="top-navbar">
         <div className="logo">
-          Retention Plan RH <span className="highlight"> AI-Advisor</span>
+          Retention Plan RH <span className="highlight">AI-Advisor</span>
           <p>Analyse prédictive de rétention des talents</p>
         </div>
-        
-          <div className="logout-button">
-           <LogoutButton />
-         </div>
-          
+        <div className="logout-button">
+          <LogoutButton />
+        </div>
       </header>
 
-      <div className="main-layout">
-        {/* SECTION 1 : FORMULAIRE (GAUCHE) */}
-        <aside className="profile-section">
-          <div className="section-header">
-            <h2>Profil Employé</h2>
-            <p>Saisissez les données pour l'analyse</p>
-          </div>
-          <div className="form-wrapper">
-            <EmployeeForm 
-              formData={formData} 
-              onChange={handleChange} 
-              onSubmit={handleAnalysis} 
-              loading={loading} 
-            />
-          </div>
-        </aside>
+      <div className="generate-layout">
+        <div className="top-section-tabs">
+  <button
+    className={`tab-btn ${activeSection === "Profil" ? "active" : ""}`}
+    onClick={() => setActiveSection("Profil")}
+  >
+    Profil Employé
+  </button>
 
-        {/* SECTION 2 : RÉSULTATS (DROITE) */}
-        <main className="results-section">
-          {loading && (
-            <div className="overlay-loading">
-              <div className="spinner"></div>
-              <p>Analyse IA en cours...</p>
-            </div>
-          )}
+  <button
+  className={`tab-btn ${activeSection === "Résultats" ? "active" : ""}`}
+  onClick={handleGeneratePlan}
+>
+  Génération & Prédiction
+</button>
+</div>
 
-          {result ? (
-            <div className="analysis-content">
-              {/* ZONE SCORE PROPORTIONNELLE */}
-              <div className="score-summary-card">
-                <h3>Probabilité de désengagement</h3>
-                <div className="score-viz">
-                  <div className={`score-circle ${result.score > 70 ? 'danger' : result.score > 35 ? 'warning' : 'safe'}`}>
-                    <span className="percent">{result.score?.toFixed(1)}%</span>
-                  </div>
-                  <div className="score-text">
-                    <h4>{result.score > 50 ? "Risque Critique" : "Risque Modéré"}</h4>
-                    <p>Basé sur les tendances actuelles du marché et le profil saisi.</p>
-                  </div>
-                </div>
+
+        <main className="main-content">
+  {activeSection === "Profil" && (
+    <EmployeeForm
+      formData={formData}
+      onChange={handleChange}
+      onSubmit={handleAnalysis}
+    />
+  )}
+
+  {activeSection === "Résultats" && (
+    <div className="result-section">
+      {loading && <p>Analyse IA en cours...</p>}
+
+      {result ? (
+        <div className="analysis-content">
+          {/* ZONE SCORE PROPORTIONNELLE */}
+          <div className="score-summary-card">
+            <h3>Probabilité de désengagement</h3>
+            <div className="score-viz">
+              <div className={`score-circle ${
+                result.score > 70 ? 'danger' : result.score > 35 ? 'warning' : 'safe'
+              }`}>
+                <span className="percent">{result.score?.toFixed(1)}%</span>
               </div>
-
-              {/* ZONE PLAN D'ACTION (3 BLOCS) */}
-              <div className="action-plan-container">
-                <h3 className="plan-title">Plan de Rétention Préconisé</h3>
-                <div className="actions-grid">
-                  {Array.isArray(result.plan) && result.plan.slice(0, 3).map((action, index) => (
-                    <div key={index} className="action-card">
-                      <div className="card-number">0{index + 1}</div>
-                      <h4>Action Stratégique</h4>
-                      <p>{action}</p>
-                    </div>
-                  ))}
-                </div>
+              <div className="score-text">
+                <h4>{result.score > 50 ? "Risque Critique" : "Risque Modéré"}</h4>
+                <p>Basé sur les tendances actuelles du marché et le profil saisi.</p>
               </div>
             </div>
-          ) : (
-            <div className="empty-placeholder">
-              <div className="placeholder-icon">🔍</div>
-              <h2>Prêt pour l'analyse</h2>
-              <p>Soumettez le profil à gauche pour générer le score et le plan d'action.</p>
+          </div>
+
+          {/* ZONE PLAN D'ACTION (3 BLOCS) */}
+          <div className="action-plan-container">
+            <h3 className="plan-title">Plan de Rétention Préconisé</h3>
+            <div className="actions-grid">
+              {Array.isArray(result.plan) && result.plan.slice(0, 3).map((action, index) => (
+                <div key={index} className="action-card">
+                  <div className="card-number">0{index + 1}</div>
+                  <h4>Action Stratégique</h4>
+                  <p>{action}</p>
+                </div>
+              ))}
             </div>
-          )}
-        </main>
+          </div>
+        </div>
+      ) : (
+        <div className="empty-placeholder">
+          <div className="placeholder-icon">🔍</div>
+          <h2>Prêt pour l'analyse</h2>
+          <p>Soumettez le profil à gauche pour générer le score et le plan d'action.</p>
+        </div>
+      )}
+    </div>
+      )}
+     </main>
+
       </div>
     </div>
   );
 };
+
 export default Generate;
